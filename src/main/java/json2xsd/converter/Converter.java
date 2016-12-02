ae59e616b2f925707543911b2b41ec64bf239afd
giwrgosrtheod <giwrgosrtheod@gmail.com>
2016-12-02 22:16:28 +0200

    Update Converter.java

--- src/main/java/json2xsd/converter/Converter.java
+++ src/main/java/json2xsd/converter/Converter.java
@@ -17,12 +17,12 @@ public class Converter {
     {
     	//The following commented out lines are used for testing in Eclipse :
     	//InputStream inputStream = new FileInputStream("src/main/java/resources/jsonSchema.json");
-		//Reader jsonSchema = new InputStreamReader(inputStream);
-		Reader jsonSchema = new InputStreamReader(System.in);
-		Document doc = Jsons2Xsd.convert(jsonSchema, "json2xsd", OuterWrapping.ELEMENT, "json2xsd");
-		String temp = XmlUtil.asXmlString(doc.getDocumentElement());		
-		PrintWriter out = new PrintWriter(new FileOutputStream("xsdSchema.xml", true),  true);
-		out.println(temp);
-		System.out.println("The file xsdSchema.xml is ready.");
+	//Reader jsonSchema = new InputStreamReader(inputStream);
+	Reader jsonSchema = new InputStreamReader(System.in);
+	Document doc = Jsons2Xsd.convert(jsonSchema, "json2xsd", OuterWrapping.ELEMENT, "json2xsd");
+	String temp = XmlUtil.asXmlString(doc.getDocumentElement());		
+	PrintWriter out = new PrintWriter(new FileOutputStream("xsdSchema.xml", true),  true);
+	out.println(temp);
+	System.out.println("The file xsdSchema.xml is ready.");
     }    
 }
